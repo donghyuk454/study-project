0f09d9cca7237d869fe24f01a2983728553a10ce
dongdong <donghyuk454@gmail.com>
2022-06-30 22:04:43 +0900

    의존성 주입, db 정보 입력

--- src/main/java/dong/study/studyproject/SpringConfig.java
+++ src/main/java/dong/study/studyproject/SpringConfig.java
@@ -1,13 +1,23 @@
 package dong.study.studyproject;
 
+import dong.study.studyproject.repository.JdbcMemberRepository;
 import dong.study.studyproject.repository.MemberRepository;
-import dong.study.studyproject.repository.MemoryMemberRepository;
 import dong.study.studyproject.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
