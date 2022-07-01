8346bdcf1ebd2f1aecd7e2787e4c8cf047af1d98
dongdong <donghyuk454@gmail.com>
2022-07-01 18:59:02 +0900

    스프링 데이터 jpa 를 활용한 repository 작성

--- src/main/java/dong/study/studyproject/SpringConfig.java
+++ src/main/java/dong/study/studyproject/SpringConfig.java
@@ -1,42 +1,44 @@
 package dong.study.studyproject;
 
-import dong.study.studyproject.repository.JdbcMemberRepository;
-import dong.study.studyproject.repository.JdbcTemplateMemberRepository;
-import dong.study.studyproject.repository.JpaMemberRepository;
 import dong.study.studyproject.repository.MemberRepository;
 import dong.study.studyproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
 //    private DataSource dataSource;
-    private EntityManager em;
+//    private EntityManager em;
 
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//    this.em = em;
+//}
+
+
+    private final MemberRepository memberRepository;
+
     @Autowired
-    public SpringConfig(EntityManager em) {
-    this.em = em;
-}
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
+//    @Bean
+//    public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
